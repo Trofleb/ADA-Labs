f55ed8afc1f46eb395eca83a278135a1e072900f
Teo Stocco <teo@zifeo.com>
2016-10-15 15:10:57 +0200

    finalize isa collect

--- 02 - Data From The Web/isa_collect.py
+++ 02 - Data From The Web/isa_collect.py
@@ -1,13 +1,9 @@
 
 # coding: utf-8
 
-# # Gather Bachelor students data
+# # Collect isa data
 
-# The purpose of this script is only to collect the data of Bachelor students, group and clean it all, serialize it in order to be used by other scripts analyzing the dataset.
-
-# ## Assignment
-
-# 1) Obtain all the data for the Bachelor students, starting from 2007. Keep only the students for which you have an entry for both Bachelor semestre 1 and Bachelor semestre 6.
+# The purpose of this notebook is only to collect the isa data over students, group and clean it all, serialize it in order to be used by other scripts analyzing the dataset.
 
 # In[1]:
 
@@ -17,18 +13,22 @@ import pandas as pd
 from bs4 import BeautifulSoup
 
 
+# ## 1 General parameters
+
+# Those data were taken out from Postman or simple network query analyzer.
+
 # In[2]:
 
 ISA_url = 'http://isa.epfl.ch/imoniteur_ISAP/!gedpublicreports'
 ISA_html = ISA_url + '.html'
 ISA_filter = ISA_url + '.filter'
-ww_i_reportModel = '133685247'  # base reference from Postman
-ww_i_reportModelXsl = '133685271'  # for XLS files from Postman
+ww_i_reportModel = '133685247'
+ww_i_reportModelXsl = '133685271'
 
 
-# ### Find the parameters values
+# ## 2 Dynamic parameters
 
-# In order to find all the existing parameter names and possible values, we make a request on the form page and then parse it
+# In order to find all the existing parameter names and possible values, we make a request on the form page and then parse it.
 
 # In[3]:
 
@@ -36,7 +36,7 @@ form_html = r.get(ISA_filter, params={'ww_i_reportModel': ww_i_reportModel})
 form_bs = BeautifulSoup(form_html.content, "html.parser")
 
 
-# We parse the HTML file looking for "select" tags which correspond to the different menus, and for each of them we look at the "option" tags which are the entries. We save our results in a dictionnary
+# We parse the HTML file looking for `select` tags which correspond to the different menus, and for each of them we look at the `option` tags which are the entries.
 
 # In[4]:
 
@@ -58,12 +58,46 @@ params.set_index(['Parameter', 'Name'], inplace=True)
 params
 
 
-# ### Gather the data
+# ## 3 Select parameters
+
+# In this homework we are interesting in students from the "Informatique" section starting from 2007.
 
 # In[5]:
 
+unite = params.loc['ww_x_UNITE_ACAD', 'Informatique']
+unite
+
+
+# In[6]:
+
+years = params.loc['ww_x_PERIODE_ACAD']
+years
+
+
+# In[7]:
+
+param_semesters = params.loc['ww_x_PERIODE_PEDAGO']
+
+
+# In[8]:
+
+# do not take into account "b" semester as there are all empty (not sure of signification)
+bachelor_semesters = param_semesters[['Bachelor' in idx and 'b' not in idx for idx in param_semesters.index]]
+bachelor_semesters
+
+
+# In[9]:
+
+master_semesters = param_semesters[['Master' in idx or 'Projet' in idx for idx in param_semesters.index]]
+master_semesters
+
+
+# ## 4. Download data
+
+# In[10]:
+
 def listOf(unite, year, semester):
-    # retrieve gps reference using main form query for table access
+    # retrieve gps reference as if we submit isa form and got list of results
     gps_html = r.get(ISA_filter, params={
         'ww_b_list': 1,
         'ww_i_reportmodel': ww_i_reportModel,
@@ -77,7 +111,7 @@ def listOf(unite, year, semester):
     if len(link) != 2:
         return None
     
-    # isolate second link (first one is always all) and rip off useless part
+    # isolate second link (first one is always "Tous") and rip off useless part
     gps = int(link[1]['onclick'][21:-16])
     table_html = r.get(ISA_html, params={
         'ww_i_reportmodel': ww_i_reportModel,
@@ -87,47 +121,21 @@ def listOf(unite, year, semester):
         'ww_x_PERIODE_PEDAGO': semester,
         'ww_x_GPS': gps
     })
-    table_bs = BeautifulSoup(table_html.content, "html.parser")
-    tables = pd.read_html(str(table_bs), flavor='bs4', skiprows=[0, 1], header=0, index_col=10)
-    
+    tables = pd.read_html(table_html.content, flavor='bs4', skiprows=[0, 1], header=0, index_col=10)
     if len(tables) != 1:
         return None
     
+    # get the table and remove student names (avoid the name to be indexed by Google on Github...)
     table = tables[0]
     table.drop('Nom Prénom', axis=1, inplace=True)
     table.drop([col for col in table.columns if 'Unnamed' in col], axis=1, inplace=True)
     return table
 
 
-# In this homework we are interesting in students from the "Informatique" section, and we want the data as excel files. Therefore we give the following parameters fixed values:
-
-# In[6]:
-
-unite = params.loc['ww_x_UNITE_ACAD', 'Informatique']
-unite
-
-
-# In[7]:
-
-years = params.loc['ww_x_PERIODE_ACAD']
-years
-
-
-# In[8]:
-
-param_semesters = params.loc['ww_x_PERIODE_PEDAGO']
-
-
-# In[9]:
-
-# do not take into account "b" semester as there are all empty (not sure of signification)
-bachelor_semesters = param_semesters[['Bachelor' in idx and 'b' not in idx for idx in param_semesters.index]]
-bachelor_semesters
-
-
-# In[10]:
+# In[11]:
 
 def gather(unite, years, semesters):
+    # loop over years and semesters and assemble them into one data frame
     yearly = []
     for year, year_value in years.iterrows():
         print(year)
@@ -137,46 +145,37 @@ def gather(unite, years, semesters):
             df = listOf(unite, year_value, semester_value)
             swarm.append(df)
         yearly.append(pd.concat(swarm, keys=semesters.index))
-    return pd.concat(yearly, keys=years.index)
-
-
-# In[11]:
-
-bachelor = gather(unite, years, bachelor_semesters)
+    return pd.concat(yearly, keys=years.index, names=['Year', 'Semester', 'Sciper'])
 
 
 # In[12]:
 
-bachelor.head()
+bachelor = gather(unite, years, bachelor_semesters)
 
 
 # In[13]:
 
-bachelor.to_pickle('bachelor')
+bachelor.head()
 
 
 # In[14]:
 
-master_semesters = param_semesters[['Master' in idx or 'Projet' in idx for idx in param_semesters.index]]
-master_semesters
+master = gather(unite, years, master_semesters)
 
 
 # In[15]:
 
-master = gather(unite, years, master_semesters)
+master.head()
 
+
+# ## 5 Serialize
 
 # In[16]:
 
-master.head()
+bachelor.to_pickle('bachelor')
 
 
 # In[17]:
 
 master.to_pickle('master')
-
-
-# In[ ]:
-
-
 
