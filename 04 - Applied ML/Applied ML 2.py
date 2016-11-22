f5085f6d217b3079c5f4d2b0782e7d18f4d68703
Nicolas Casademont <nicolas.casademont@epfl.ch>
2016-11-22 21:59:56 +0100

    clean !

--- 04 - Applied ML/Applied ML 2.py
+++ 04 - Applied ML/Applied ML 2.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[192]:
+# In[ ]:
 
 get_ipython().magic('matplotlib inline')
 import pandas as pd
@@ -14,12 +14,12 @@ get_ipython().magic('autoreload 2')
 from sklearn import metrics
 
 
-# In[193]:
+# In[ ]:
 
 df = pd.read_csv("./CrowdstormingDataJuly1st.csv")
 
 
-# In[200]:
+# In[ ]:
 
 from helpers import clean_data, group_data, prep_ML, normalize
 dfc = clean_data(df)
@@ -32,31 +32,31 @@ label_true = ((y_possible['rater1'] + y_possible['rater2']) / 2 <= 0.5).values
 
 # First let's try the k-means algorithm with 2 clusters and print out the silhouette score:
 
-# In[204]:
+# In[ ]:
 
 from sklearn.cluster import KMeans
 
 
 # FYI: n_jobs = 1 because the parallel version of k-means doesn't work on OSX.
 
-# In[231]:
+# In[ ]:
 
 km = KMeans(n_clusters=2, max_iter=600, init="k-means++", n_jobs=1 )
 
 
-# In[232]:
+# In[ ]:
 
 km.fit(X)
 
 
-# In[233]:
+# In[ ]:
 
 labels = km.labels_
 s1 = metrics.silhouette_score(X, labels, metric='euclidean')
 print("s1 =", s1)
 
 
-# In[234]:
+# In[ ]:
 
 def test_km(X):
     km = KMeans(n_clusters=2, max_iter=600, init="k-means++", n_jobs=1 )
@@ -67,14 +67,14 @@ def test_km(X):
 
 # We know that the most valuable feature is the mean_Exp therefore we will remove it (just for fun)
 
-# In[253]:
+# In[ ]:
 
 from helpers import compute_feature_importance_rfc
 
 compute_feature_importance_rfc(X, label_true)
 
 
-# In[254]:
+# In[ ]:
 
 X2 = X.drop("meanExp", axis=1)
 s2 = test_km(X2)
@@ -86,7 +86,7 @@ print("s2 - s1 =", s2 - s1)
 
 # Ok now let's remove a second feature, we will follow the same intuition as before and remove the second best feature, the club information.
 
-# In[255]:
+# In[ ]:
 
 # we know from before that the mean_Exp and goal information is at position 8, and 7 respectively in the X array
 X3 = X.drop(["meanExp", "goals"], axis=1)
@@ -98,7 +98,7 @@ print("s3 - s2 =", s3 - s2)
 
 # There is an even better improvement
 
-# In[256]:
+# In[ ]:
 
 # we now remove seExp in addition to the other 2 (3rd best feature)
 X4 = X.drop(["meanExp", "goals", "seIAT"], axis=1)
@@ -111,7 +111,7 @@ print("s4 - s3 =", s4 - s3)
 
 # To see whether the clustering is close to a dark/light separation we will compute in addition to the silhouerre the accuracy score 
 
-# In[306]:
+# In[ ]:
 
 def scoring_complete(X):
     km = KMeans(n_clusters=2, max_iter=300, init="k-means++", n_jobs=1 )
@@ -125,7 +125,7 @@ scoring_complete(X)
 
 # We have a very low closeness score but at least it's positive.
 
-# In[307]:
+# In[ ]:
 
 scoring_complete(X2)
 
@@ -134,12 +134,12 @@ scoring_complete(X2)
 
 # Other example :
 
-# In[308]:
+# In[ ]:
 
 scoring_complete(X3)
 
 
-# In[309]:
+# In[ ]:
 
 scoring_complete(X4)
 
@@ -150,7 +150,7 @@ scoring_complete(X4)
 # 
 # rows : 8, 10, 11
 
-# In[310]:
+# In[ ]:
 
 X5 =  X.drop(["yellowReds", "redCards", "yellowCards"], axis=1)
 scoring_complete(X5)
@@ -160,45 +160,45 @@ scoring_complete(X5)
 
 # Let's now remove all the features that have the worst feature importance until our closeness score drops significantly
 
-# In[316]:
+# In[ ]:
 
 compute_feature_importance_rfc(X5, label_true)
 
 
-# In[336]:
+# In[ ]:
 
 X6 =  X.drop(["yellowReds", "redCards", "yellowCards", "position", "leagueCountry", "height"], axis=1)
 scoring_complete(X6)
 
 
-# In[338]:
+# In[ ]:
 
 compute_feature_importance_rfc(X6, label_true)
 
 
-# In[339]:
+# In[ ]:
 
 X7 =  X.drop(["yellowReds", "redCards", "yellowCards", "position", "leagueCountry", "height", "weight"], axis=1)
 scoring_complete(X7)
 
 
-# In[341]:
+# In[ ]:
 
 compute_feature_importance_rfc(X7, label_true)
 
 
-# In[342]:
+# In[ ]:
 
 X8 =  X.drop(["yellowReds", "redCards", "yellowCards", "position", "leagueCountry", "height", "weight", "defeats"], axis=1)
 scoring_complete(X8)
 
 
-# In[343]:
+# In[ ]:
 
 compute_feature_importance_rfc(X8, label_true)
 
 
-# In[344]:
+# In[ ]:
 
 X9 =  X.drop(["yellowReds", "redCards", "yellowCards", "position", "leagueCountry", "height", "weight", "defeats", "ties"], axis=1)
 scoring_complete(X9)
@@ -206,7 +206,7 @@ scoring_complete(X9)
 
 # We stop here as we have the worst result.
 
-# In[349]:
+# In[ ]:
 
 plt.scatter(X8["club"], X8["meanExp"], c=labels)
 
