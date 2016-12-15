83dd769a207a5625e892d39c6d085fe34f0e7b79
Yann Dupont <yaanst@gmail.com>
2016-12-15 17:48:44 +0100

    Merged all parts together

--- 05 - Taming Text/Hw 05.py
+++ 05 - Taming Text/Hw 05.py
@@ -3,7 +3,7 @@
 
 # ## Some useful imports
 
-# In[158]:
+# In[3]:
 
 import nltk
 import re
@@ -20,19 +20,19 @@ from gensim.models import LdaModel
 
 # ## Getting email text
 
-# In[159]:
+# In[5]:
 
 emails = pd.read_csv("./hillary-clinton-emails/Emails.csv", usecols=["RawText"])
 
 
-# In[160]:
+# In[3]:
 
 emails.head()
 
 
 # Concatenating all the e-mails into one big text.
 
-# In[161]:
+# In[6]:
 
 texts = [text for text in emails.RawText]
 full_text = " ".join(texts)
@@ -40,7 +40,7 @@ full_text = " ".join(texts)
 
 # ### First raw word count 
 
-# In[162]:
+# In[7]:
 
 def show_wordCloud(text):
     # lower max_font_size
@@ -83,7 +83,7 @@ show_wordCloud(full_text)
 # - stemming
 # - url/email removal: although they not appear in the world cloud url are often present in mails and can be blacklisted with `./_@:` which are usally part of desired words
 
-# In[166]:
+# In[8]:
 
 def lower_case(texts_tokenized):
     new_tokens = []
@@ -92,7 +92,7 @@ def lower_case(texts_tokenized):
     return new_tokens
 
 
-# In[168]:
+# In[9]:
 
 def remove_stop_words(texts_tokenized):
     new_tokens = []
@@ -101,7 +101,7 @@ def remove_stop_words(texts_tokenized):
     return new_tokens
 
 
-# In[170]:
+# In[10]:
 
 def lemmatization(texts_tokenized):
     new_tokens = []
@@ -111,7 +111,7 @@ def lemmatization(texts_tokenized):
     return new_tokens
 
 
-# In[172]:
+# In[11]:
 
 def stemming(texts_tokenized):
     new_tokens = []
@@ -121,7 +121,7 @@ def stemming(texts_tokenized):
     return new_tokens
 
 
-# In[174]:
+# In[12]:
 
 def remove_special_chars(texts_tokenized):
     blacklist_regex = r'\W' # Matches every token that is a non-alphanumeric character
@@ -131,7 +131,7 @@ def remove_special_chars(texts_tokenized):
     return new_tokens
 
 
-# In[176]:
+# In[13]:
 
 def remove_email_terminology(texts_tokenized):
     blacklist_terms = ['subject', 'cc', 'cci', 'sent', 'date', 're', 'mailto']
@@ -141,7 +141,7 @@ def remove_email_terminology(texts_tokenized):
     return new_tokens
 
 
-# In[180]:
+# In[14]:
 
 texts_tokenized = [nltk.word_tokenize(text) for text in texts]
 texts_tokenized = lower_case(texts_tokenized)
@@ -152,15 +152,15 @@ texts_tokenized = remove_special_chars(texts_tokenized)
 texts_tokenized = remove_email_terminology(texts_tokenized)
 
 
-# In[181]:
+# In[15]:
 
 texts2 = [" ".join(tokens) for tokens in texts_tokenized]
 full_text2 = " ".join(texts2)
 
 
-# In[182]:
+# In[16]:
 
-show_wordCloud(full_text)
+show_wordCloud(full_text2)
 
 
 # The result has improved a lot, however there are still issues:
@@ -181,7 +181,7 @@ show_wordCloud(full_text)
 
 # One could also try to keep only (proper) nouns as verbs, adjectives and adverbs might have a very generic meaning (e.g. also, send). Using a POS tagging just after tokenization, we can discard the others.
 
-# In[184]:
+# In[17]:
 
 def keep_nouns(texts_tokenized):
     to_keep_tag = ['NN', 'NNS', 'NNP', 'NNPS']
@@ -193,7 +193,7 @@ def keep_nouns(texts_tokenized):
     return new_tokens
 
 
-# In[185]:
+# In[18]:
 
 texts_tokenized2 = [ nltk.word_tokenize(text) for text in texts]
 texts_tokenized2 = keep_nouns(texts_tokenized2)
@@ -202,7 +202,7 @@ texts_tokenized2 = remove_stop_words(texts_tokenized2)
 texts_tokenized2 = lemmatization(texts_tokenized2)
 
 
-# In[186]:
+# In[19]:
 
 # Changing variable name so that we can use texts_tokenized2 without stemming in Part 3
 texts_tokenized2_clean = stemming(texts_tokenized2)
@@ -210,18 +210,359 @@ texts_tokenized2_clean = remove_special_chars(texts_tokenized2_clean)
 texts_tokenized2_clean = remove_email_terminology(texts_tokenized2_clean)
 
 
-# In[193]:
+# In[20]:
 
 texts3 = [" ".join(tokens) for tokens in texts_tokenized2_clean]
 full_text3 = " ".join(texts3)
 
 
-# In[194]:
+# In[21]:
 
 show_wordCloud(full_text3)
 
 
 # Although this is more aggressive than the previous trials, this world cloud gives a better overview of the general content. The non-nouns are might be due to english verbs/nouns duality.
+
+# We wanted here to achieve some sort of Luhn's analysis. We could also have taken word frequencies for each document and only kept the words which are not frequent in all mails.
+
+# In[18]:
+
+from IPython.display import Image
+Image(filename="./luhn's analysis.png")
+
+
+# ## Part 2
+
+# In[78]:
+
+import pycountry
+
+
+# We are going to use the  sentiment analyser from Vader first. After looking at the results of the demo function we will be using the compound part of the result which gives the entensity of the sentiment and is positive or negative with respect to the type of sentiment (positive, negative)
+
+# We confirmed our choice with this source.
+# https://www.linkedin.com/pulse/sentiment-analysis-using-vader-muthuraj-kumaresan
+
+# In[363]:
+
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+
+# In[364]:
+
+sid = SentimentIntensityAnalyzer()
+
+
+# We compute for each mail the sentiment analysis and store it in a csv file so that we don't have to redo it (very slow...)
+
+# In[286]:
+
+sentiment_df = pd.DataFrame(columns=["compound", "neutral", "negative", "positive"])
+
+import progressbar
+
+bar = progressbar.ProgressBar(max_value=len(emails.RawText))
+
+i = 0
+for email in emails.RawText:
+    bar.update(i)
+    score = sid.polarity_scores(email)
+    sentiment_df.loc[i] = (score["compound"], score["neu"], score["neg"], score["pos"])
+    i += 1
+
+
+# In[287]:
+
+sentiment_vader_df.to_csv("sentiment_vader.csv")
+
+
+# We read it back from the csv.
+
+# In[365]:
+
+sentiment_vader_df = pd.read_csv("./sentiment_vader.csv", index_col=0)
+
+
+# In[366]:
+
+sentiment_vader_df.describe()
+
+
+# We process the dataset for the contries so that we can use it easily afterwards.
+
+# In[367]:
+
+countries = [(country.name.lower(), country.alpha_2.lower(), country.alpha_3.lower()) for country in pycountry.countries]
+
+
+# we initialize a dataframe for the sentiment per country
+
+# In[368]:
+
+sentiment_per_country = pd.DataFrame(columns=["country", "pos","neg"]).set_index("country")
+
+
+# In[369]:
+
+for name, _, _ in countries:
+    sentiment_per_country.loc[name] = 0
+
+
+# we populate it with the sentiment per mail that contains the country, you may notice that this is not the best way to do things as the country may not be related to the sentiment of the whole mail doing it by sentence would be more fair but it would be much more long to compute.
+
+# In[381]:
+
+import progressbar
+
+bar = progressbar.ProgressBar(max_value=len(emails.RawText))
+
+i = 0
+for email in emails.RawText:
+    bar.update(i)
+    add = 0
+    sentiment = sentiment_vader_df.loc[i]["compound"]
+    
+    tokens = nltk.word_tokenize(email)
+    nouns = keep_nouns([tokens])[0]
+    country_found = []
+    # find countries
+    for noun in nouns:
+        lc_noun = noun.lower()
+        index = 0
+        for name, a2, a3 in countries:
+            if name not in country_found and (lc_noun == name or lc_noun == a2 or lc_noun == a3):
+                country_found.append(name)
+                if sentiment >= 0:
+                    sentiment_per_country.pos.loc[name] += 1
+                if sentiment < 0:
+                    sentiment_per_country.neg.loc[name] += 1
+    i += 1
+
+
+# In[382]:
+
+sentiment_per_country.describe()
+
+
+# At this point you may realise that we have huge values in the max for both pos and neg, we will see this in more details a bit later.
+
+# Because we have a lot of countries and we want the graph to readable we will only show the most important countries
+
+# In[383]:
+
+quantile = 0.9
+
+
+# In[384]:
+
+(1 - quantile)* len(countries)
+
+
+# In[385]:
+
+sentiment_per_country_pos = sentiment_per_country[(sentiment_per_country.pos > sentiment_per_country.pos.quantile(quantile)) | (sentiment_per_country.neg > sentiment_per_country.neg.quantile(quantile))]
+
+
+# In[386]:
+
+sentiment_per_country_pos.shape
+
+
+# In[387]:
+
+sentiment_per_country_pos.plot.bar(y="pos")
+sentiment_per_country_pos.plot.bar(y="neg", color="r")
+plt.show()
+
+
+# We see here a problem with our simple implementation. There are some countries who are much higher than they should be. To show why let's look at their names and alpha2 : 
+# 
+# Armenia -> am, 
+# 
+# Cocos (keeling) island -> CC, 
+# 
+# India -> in, 
+# 
+# Réunion -> re, 
+# 
+# Saint Pierre and Miquelon -> pm 
+# 
+
+# There are very common words in english that don't seem to be taken in account in the noun filter we have. We found out that it was because they were in caps.
+# 
+# We decided to remove those words completly as these countries (maybe not India) are not so relevant than the other mentioned for example the US.
+# 
+# Therefore if you search for the feeling of Hilary Clinton towards these countries specifically please discard this study.
+
+# In[275]:
+
+for name, _, _ in countries:
+    sentiment_per_country.loc[name] = 0
+
+
+# In[312]:
+
+bar = progressbar.ProgressBar(max_value=len(emails.RawText))
+
+i = 0
+for email in emails.RawText:
+    bar.update(i)
+    add = 0
+    sentiment = sentiment_vader_df.loc[i]["compound"]
+    
+    tokens = nltk.word_tokenize(email.lower())
+    nouns = keep_nouns([tokens])[0]
+    nouns = nouns[np.in1d(nouns, ["am","cc","re","in","pm"], invert=True)]
+    country_found = []
+    # find countries
+    for noun in nouns:
+        index = 0
+        for name, a2, a3 in countries:
+            if name not in country_found and (noun == name or noun == a2 or noun == a3):
+                country_found.append(name)
+                if sentiment >= 0:
+                    sentiment_per_country.pos.loc[name] += 1
+                if sentiment < 0:
+                    sentiment_per_country.neg.loc[name] += 1
+    i += 1
+
+
+# In[356]:
+
+sentiment_per_country.describe()
+
+
+# In[360]:
+
+sentiment_per_country_pos = sentiment_per_country[(sentiment_per_country.pos > sentiment_per_country.pos.quantile(quantile)) | (sentiment_per_country.neg > sentiment_per_country.neg.quantile(quantile))]
+
+
+# In[361]:
+
+sentiment_per_country_pos.shape
+
+
+# In[362]:
+
+sentiment_per_country_pos.plot.bar(y="pos")
+sentiment_per_country_pos.plot.bar(y="neg", color="r")
+plt.show()
+
+
+# With this we have a much cleaner look at the frequencies of positive and negative emails of Clinton
+
+# In[389]:
+
+from nltk.corpus import opinion_lexicon
+pos_lex = opinion_lexicon.positive()
+neg_lex = opinion_lexicon.negative()
+
+
+# In[230]:
+
+sentiment_Liu_Hu_df = pd.DataFrame(columns=["sentiment", "count_pos", "count_neg"])
+bar = progressbar.ProgressBar(max_value=len(emails.RawText))
+
+i = 0
+for email in emails.RawText:
+    bar.update(i)
+    tokens = nltk.word_tokenize(email)
+    count_pos = np.sum(np.in1d(tokens, pos_lex))
+    count_neg = np.sum(np.in1d(tokens, neg_lex))
+    
+    if count_pos > count_neg:
+        sent = 1
+    else:
+        sent = -1
+    
+    sentiment_df.loc[i] = (sent, count_pos, count_neg)
+    i += 1
+
+
+# In[ ]:
+
+sentiment_Liu_Hu_df.to_csv("sentiment_Liu_Hu.csv")
+
+
+# We now do the same for the Liu Hu method which is "just" a count of positive and negative word in precomputed lexicon.
+
+# In[390]:
+
+sentiment_Liu_Hu_df = pd.read_csv("./sentiment_Liu_Hu.csv", index_col=0)
+
+
+# In[391]:
+
+sentiment_Liu_Hu_df.describe()
+
+
+# In[392]:
+
+countries = [(country.name.lower(), country.alpha_2.lower(), country.alpha_3.lower()) for country in pycountry.countries]
+
+
+# In[393]:
+
+sentiment_per_country = pd.DataFrame(columns=["country", "pos","neg"]).set_index("country")
+
+
+# In[394]:
+
+for name, _, _ in countries:
+    sentiment_per_country.loc[name] = 0
+
+
+# In[338]:
+
+i = 0
+bar = progressbar.ProgressBar(max_value=len(emails.RawText))
+
+for email in emails.RawText:
+    bar.update(i)
+    add = 0
+    sentiment = sentiment_Liu_Hu_df.loc[i]["sentiment"]
+    
+    tokens = nltk.word_tokenize(email.lower())
+    nouns = keep_nouns([tokens])[0]
+    nouns = nouns[np.in1d(nouns, ["am","cc","re","in","pm"], invert=True)]
+    country_found = []
+    # find countries
+    for noun in nouns:
+        lc_noun = noun.lower()
+        index = 0
+        for name, a2, a3 in countries:
+            if name not in country_found and (lc_noun == name or lc_noun == a2 or lc_noun == a3):
+                country_found.append(name)
+                if sentiment >= 0:
+                    sentiment_per_country.pos.loc[name] += 1
+                if sentiment < 0:
+                    sentiment_per_country.neg.loc[name] += 1
+    i += 1
+
+
+# In[339]:
+
+sentiment_per_country.describe()
+
+
+# In[351]:
+
+sentiment_per_country_pos = sentiment_per_country[(sentiment_per_country.pos > sentiment_per_country.pos.quantile(quantile)) | (sentiment_per_country.neg > sentiment_per_country.neg.quantile(quantile))]
+
+
+# In[352]:
+
+sentiment_per_country_pos.shape
+
+
+# In[353]:
+
+sentiment_per_country_pos.plot.bar(y="pos")
+sentiment_per_country_pos.plot.bar(y="neg", color="r")
+plt.show()
+
+
+# And here we have the analysis with the Liu Hu method which is very similar to the one we had before.
 
 # # 3. Topic modelling
 
@@ -229,7 +570,7 @@ show_wordCloud(full_text3)
 
 # To get meaningful topics, we remove the words that occur in almost every e-mail and that do not convey information about the topic discussed in it (e.g words from the banner present in many of them: "unclassified u.s department of state...")
 
-# In[196]:
+# In[22]:
 
 def remove_unwanted_tokens(tokens):
     # Regular expression matching all single non-alphanumeric characters (\W), 
@@ -248,7 +589,7 @@ def remove_unwanted_tokens(tokens):
 
 # We skipped the stemming step in order to keep a big enough part of the words. That way we will be able to understand their actual meaning and origin.
 
-# In[197]:
+# In[23]:
 
 texts_tokenized3 = remove_unwanted_tokens(texts_tokenized2)
 
@@ -257,12 +598,12 @@ texts_tokenized3 = remove_unwanted_tokens(texts_tokenized2)
 # It seems like we lose many words, but it is actually only a few unique words appearing many times as well as some tokens smaller than 3 characters and the non-alphanumeric ones. This is fine as they do not add meaning to the e-mail. <br />
 # The dictionnary we build later on has approximately 2000 tokens less after applying that function than before (from ~37500 tokens to ~35500 tokens)
 
-# In[198]:
+# In[24]:
 
 ' '.join(texts_tokenized2[0])
 
 
-# In[199]:
+# In[25]:
 
 ' '.join(texts_tokenized3[0])
 
@@ -271,7 +612,7 @@ texts_tokenized3 = remove_unwanted_tokens(texts_tokenized2)
 
 # We create the dictionnary from the processed e-mail texts using gensim.corpora.dictionary.Dictionary 
 
-# In[200]:
+# In[26]:
 
 corpus_dictionary = Dictionary(texts_tokenized3)
 print(corpus_dictionary)
@@ -279,7 +620,7 @@ print(corpus_dictionary)
 
 # With that dictionnary we create a corpus made of the bag-of-words representation of each text
 
-# In[201]:
+# In[27]:
 
 corpus = [corpus_dictionary.doc2bow(tokens) for tokens in texts_tokenized3]
 
@@ -289,22 +630,24 @@ corpus = [corpus_dictionary.doc2bow(tokens) for tokens in texts_tokenized3]
 # Next, we train an Lda model on that corpus using gensim.models.LdaModel and the previously built dictionary. <br />
 # We set the hyperparameters alpha and eta to 'auto' so that gensim manages and adapts them to our data. We also do 5 passes on the corpus to improve the correctness/accuracy of the topics.
 
-# In[204]:
+# In[28]:
 
-#model = LdaModel(corpus, id2word=corpus_dictionary, num_topics=42, passes=5, alpha='auto', eta='auto', minimum_probability=0.02)
-
-
-# In[ ]:
-
-model = LdaModel.load('best_model/best_model')
+model = LdaModel(corpus, id2word=corpus_dictionary, num_topics=42, passes=5, alpha='auto', eta='auto', minimum_probability=0.02)
 
 
-# In[ ]:
+# In[29]:
 
 model.save('best_model/best_model')
 
 
-# In[206]:
+# In[30]:
+
+model = LdaModel.load('best_model/best_model')
+
+
+# We find that the topics the most understandable at first sight are the numbers 3, 41, 19, 16, 13, 18, 24, 8, 14, 36
+
+# In[32]:
 
 model.show_topics(num_topics=20, num_words=10, formatted=False)
 
